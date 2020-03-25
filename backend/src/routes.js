eb306bf8721a9e98d93d158b8d37496dd72445a9
ItsJuniorDias <juniordias_@live.com>
2020-03-25 13:15:30 -0300

    Create First Ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,21 +4,21 @@ const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) => {
+routes.post('/ongs', async (request, response) => {
   const { name, email, whatsapp, city, uf} = request.body; 
   
-  const id = crypto.ramdomBytes(4).toString('HEX');
+  const id = crypto.randomBytes(4).toString('HEX');
 
-  connection('ongs').insert({ 
+  await connection('ongs').insert({ 
     id, 
     name,
     email,
     whatsapp,
     city, 
     uf
-  })
+  });
 
-  return response.json();
+  return response.json({ id });
 });
 
 module.exports = routes;

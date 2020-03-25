b16f6560ec91f3ed286c1e86fa5fc45e51e3f1fc
ItsJuniorDias <juniordias_@live.com>
2020-03-25 14:12:12 -0300

    Incidents Delete success

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,8 @@ const routes = express.Router();
 routes.get('/ongs', OngController.index);
 routes.post('/ongs',OngController.create );
 
+routes.get('/incidents', IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id',IncidentsController.delete);
 
 module.exports = routes;

3249fc257549155e12e8fb6649e8a1efe497b95c
ItsJuniorDias <juniordias_@live.com>
2020-03-26 20:35:26 -0300

    Page login success

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -16,7 +16,7 @@ export default function Login() {
           <h1>Faça seu login</h1>
 
           <input placeholder="Sua ID" />
-          <button type="submit">Entrar</button>
+          <button className="button" type="submit">Entrar</button>
 
           <a href="/register">
             <FiLogIn size={16} color="#E02041" /> 

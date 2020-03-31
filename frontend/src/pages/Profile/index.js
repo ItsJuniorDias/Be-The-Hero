1094be2dac4a39cd092091bf7e3aece48f53edaa
ItsJuniorDias <juniordias_@live.com>
2020-03-30 21:15:31 -0300

    Success Header

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,7 +12,11 @@ export default function Profile() {
       <header>
         <img src={logoImg} alt="Be the Hero" />
         <span>Bem vinda, APAD</span>
-
+        
+        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+        <button type="button">
+          <FiPower size={18} color="#e02041" />
+         </button>
       </header>
     </div>
   );

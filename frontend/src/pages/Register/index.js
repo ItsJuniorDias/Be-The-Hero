83e043ff18c58012c59d25f7e5c65eebc9e1eedb
ItsJuniorDias <juniordias_@live.com>
2020-04-03 01:02:46 -0300

    Create State in Register component

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  function handleRegister(event) {
+    event.preventDefault();
+  }
+
   return (
    <div className='register-container'>
      <div className='content'>
         <section>
           <img src={logoImg} alt="Be the Hero" />
+
           <h1> Cadastro </h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
             os casos da sua ONG.  
@@ -24,8 +36,12 @@ export default function Register() {
           </Link>
         
         </section>
-        <form> 
-          <input placeholder="Nome da ONG" />
+        <form onSubmit={handleRegister}> 
+          <input 
+           placeholder="Nome da ONG" 
+           value={name}
+           onChange={event => setName(event.target.value)}
+           />
           <input type="email" placeholder="E-mail" />
           <input placeholder="WhatsApp" />
            

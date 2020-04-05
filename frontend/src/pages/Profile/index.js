a932e6b4fd0c5517a0b11d2cd36cb47c1d850ca5
ItsJuniorDias <juniordias_@live.com>
2020-04-04 22:21:21 -0300

    Delete incidents

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -31,6 +31,8 @@ export default function Profile() {
           Authorization: ongId, 
         }
       });
+
+      setIncidents(incidents.filter(incident => incident.id !== id ));
     } catch(err) {
       alert('Erro ao deletar caso, tente novamente.');
     }

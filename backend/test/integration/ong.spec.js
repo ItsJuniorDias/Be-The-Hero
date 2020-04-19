e27bf67dad014265fe92278aee083c680fc98bc0
ItsJuniorDias <juniordias_@live.com>
2020-04-19 20:57:15 -0300

    Integration Test

--- backend/test/integration/ong.spec.js
+++ backend/test/integration/ong.spec.js
@@ -1,16 +1,24 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.latest();
+  });
+
   it('should be able to create a new ONG', async () => {
     const response = await request(app)
     .post('/ongs')
     .send({
       name: "APAD2",
       email: "contato@gmail.com",
-      whatsapp: "470000000",
+      whatsapp: "4170000000",
       city: "São paulo",
       uf: "SP"
     });
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
   })
 }); 

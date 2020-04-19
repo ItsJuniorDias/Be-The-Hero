ff6e63d38a1f5c874c38c5eb058c29aa5fb44f77
ItsJuniorDias <juniordias_@live.com>
2020-04-19 17:42:21 -0300

    Test with Jest

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,11 +1,17 @@
-const crypto = require('crypto');
+const gerenateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
+  async index(request, response) {
+    const ongs = await connection('ongs').select('*');
+  
+    return response.json(ongs);
+  },
+
   async create(request, response) {
       const { name, email, whatsapp, city, uf} = request.body; 
       
-      const id = crypto.randomBytes(4).toString('HEX');
+      const id = gerenateUniqueId();
     
       await connection('ongs').insert({ 
         id, 
@@ -18,11 +24,4 @@ module.exports = {
     
       return response.json({ id });
     },
-
-
-  async index(request, response) {
-    const ongs = await connection('ongs').select('*');
-  
-    return response.json(ongs);
-  }
 };
